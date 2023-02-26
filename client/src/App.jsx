415df32bfd91880da424de1c49f78012d07816eb
suryaadev <rsurya1760@gmail.com>
2023-02-26 14:26:31 +0530

    frontend completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import {logo} from './assets/index'
+import {CreatePost, Home} from './pages'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-      <h1 className='text-3xl'>App is here</h1>
-    </div>
+    <BrowserRouter>
+      <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
+        <Link to="/">
+          <img src={logo} alt="logo" className='w-28 object-contain'/>
+        </Link>
+        <Link to="/create-post" className='font-inter font-medium bg-[#6469ff] text-white px-4 rounded-md py-2'>Create</Link>
+      </header>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh - 73px)]'>
+        <Routes>
+          <Route path='/' element={< Home />} />
+          <Route path='/create-post' element={<CreatePost />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   )
 }
 
 export default App
+ 
